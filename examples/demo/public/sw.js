6add83502f9a2b21ea23650d89591a8a08e28bda
Hansel-Christopher <chris23han@gmail.com>
2019-12-27 20:00:33 +0530

    initial

---
+++ examples/demo/public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('fetch', function(e){
+    //console.log(e.request.url);
+
+    // if(e.request)
+    // e.respondWith(
+    //     fetch(e.request)
+    //         .then((response) => {
+    //             if (!response.ok) console.log(e.request);
+    //         })
+    //         .catch((err) => {
+    //             console.log("offline?");
+
+    //         })
+    // )
+});
